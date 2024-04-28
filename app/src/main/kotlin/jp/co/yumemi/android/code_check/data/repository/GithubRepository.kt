8358833ff93026b8882416595b7b57d7082907ce
shunkat <shunkat@icloud.com>
2024-04-28 10:06:34 +0900

    #18 使用言語の欄の表示方法を他の情報と合わせて、nullだった場合は欄ごと表示しないようにする

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/GithubRepository.kt
@@ -8,7 +8,6 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
-import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.data.model.RepositoryInfo
 import org.json.JSONObject
 
@@ -31,7 +30,7 @@ class GithubRepository(private val context: Context) {
             items.optJSONObject(i)?.let { jsonItem ->
                 val repositoryAndOwnerName = jsonItem.optString("full_name")
                 val ownerAvatarUrl = jsonItem.getJSONObject("owner").optString("avatar_url")
-                val language = jsonItem.optString("language")
+                val language = jsonItem.opt("language") as? String
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
                 val forksCount = jsonItem.optLong("forks_count")
@@ -40,7 +39,7 @@ class GithubRepository(private val context: Context) {
                 RepositoryInfo(
                     repositoryAndOwnerName = repositoryAndOwnerName,
                     ownerAvatarUrl = ownerAvatarUrl,
-                    language = context.getString(R.string.pattern_writtern_language, language),
+                    language = language,
                     stargazersCount = stargazersCount,
                     watchersCount = watchersCount,
                     forksCount = forksCount,
