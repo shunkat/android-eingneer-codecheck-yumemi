29839db18d1ebcb96c36743e1ebf6298189549b1
shunkat <shunkat@icloud.com>
2024-04-28 16:29:55 +0900

    既存のuiテストが通るように修正

--- app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("jp.co.yumemi.android.codecheck", appContext.packageName)
+        assertEquals("jp.co.yumemi.android.code_check", appContext.packageName)
     }
 }
